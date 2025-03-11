0f5dcb4bf952c960d06f0613145aa75809f37796
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2025-03-12 01:22:49 +0900

    initial commit

---
+++ src/test/java/com/sprain6628/cicd_practice/CicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprain6628.cicd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
